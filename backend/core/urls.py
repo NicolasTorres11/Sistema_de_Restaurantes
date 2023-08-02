17ebd6489e0e806f527dc7d8dde9306612279fa6
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-01 21:48:39 -0500

    main

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swager-view'),
-    path("redoc/", schema_view.with_ui('redoc',
+    path("readocs/", schema_view.with_ui('redoc',
          cache_timeout=0), name="schema-redoc")
 ]
