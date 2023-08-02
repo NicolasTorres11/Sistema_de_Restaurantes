e8396f573acadfab6a8ea9238ad4777d8a6a03f4
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-02 14:32:48 -0500

    main

--- backend/users/api/views/views_user.py
+++ backend/users/api/views/views_user.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from django.contrib.auth.hashers import make_password
 from users.api.serializers.user_serializers import UserSerializer
 from users.models import User
 
@@ -9,3 +10,16 @@ class UserAPIView(ModelViewSet):
     permission_classes = [IsAdminUser]
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        request.data['password'] = make_password(request.data['password'])
+        return super().create(request, *args, **kwargs)
+    
+    def partial_update(self, request, *args, **kwargs):
+        password = request.data['password']
+        if password:
+            request.data['password'] = make_password(request.data['password'])
+        else:
+            request.data['password'] = request.user.password
+        return super().update(request, *args, **kwargs)
+            
