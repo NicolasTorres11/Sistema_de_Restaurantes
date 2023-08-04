81b0a6fe3ac149787899c0331378f323fa83e4e5
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-04 13:10:23 -0500

    main

--- backend/users/api/routers.py
+++ backend/users/api/routers.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from users.api.views.views_user import UserAPIView, UserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 
 router = DefaultRouter()
@@ -10,5 +14,7 @@ router.register(
 )
 
 urlpatterns = router.urls + [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh_token/', TokenRefreshView.as_view(), name='token_refre'),
     path('auth/me/', UserView.as_view(), name='Me'),
 ]
