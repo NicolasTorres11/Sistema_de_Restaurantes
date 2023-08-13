2f1818aae14bf5064485d974c3dc746267a8b2dc
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-13 14:30:47 -0500

    main

--- frontend/src/api/categories.js
+++ frontend/src/api/categories.js
@@ -2,7 +2,7 @@ import { BASE_API } from "../utils/constants";
 
 export async function getCategoriesApi() {
   try {
-    const url = `${BASE_API}/api/Categories/`;
+    const url = `${BASE_API}api/Categories/`;
     const response = await fetch(url);
     const result = await response.json();
     return result;
