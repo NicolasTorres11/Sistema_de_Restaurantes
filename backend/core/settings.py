92e3ee1d3b57f6a86a4718ac6be4ba4dd22399c3
NicolasTorres11 <99929882+NicolasTorres11@users.noreply.github.com>
2023-08-21 17:25:27 -0500

    main

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,6 +23,7 @@ DJANGO_APPS = [
 MY_APPS = [
     'users',
     'categories',
+    'products'
 ]
 
 THRID_PARTY_APPS = [
---
+++ backend/products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models.fields.files import ImageField
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Titulo')
+    image = models.ImageField(upload_to='products', verbose_name='Imagen')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_active = models.BooleanField(default=False)
+    category = models.ForeignKey('categories.Category', on_delete=models.SET_NULL, null=True)
+    
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        
+    def __str__(self) -> str:
+        return self.title
+    
