a37e2a93c578c388b2c38994df09bdf0134c7a50
Nicolas Peña <99929882+NicolasTorres11@users.noreply.github.com>
2023-09-09 20:26:57 -0500

    main

--- frontend/src/components/Admin/Products/AddEditProducts/AddEditProducts.js
+++ frontend/src/components/Admin/Products/AddEditProducts/AddEditProducts.js
@@ -1,11 +1,25 @@
-import React, { useCallback, useState } from "react";
-import { Form, Image, Button, Checkbox } from "semantic-ui-react";
+import React from "react";
+import { Form, Image, Button, Dropdown, Checkbox } from "semantic-ui-react";
 import "./AddEditProducts.scss";
 
 export function AddEditProducts() {
   return (
-    <div>
-      <h1>AddEditProducts</h1>
-    </div>
+    <Form className="add-edit-product-form">
+      <Form.Input name="title" placeholder="Nombre del Producto" />
+      <Form.Input
+        type="number"
+        name="price"
+        placeholder="Precio del Producto"
+      />
+      <Dropdown placeholder="Categoria" fluid selection search />
+      <Button type="button" fluid>
+        Subir Imagen
+      </Button>
+      <div className="add-edit-product-form__active">
+        <Checkbox toggle />
+        Estado del Producto
+      </div>
+      <Button type="submit" primary fluid content="Agregar Producto" />
+    </Form>
   );
 }
--- frontend/src/pages/Admin/ProductsAdmin.js
+++ frontend/src/pages/Admin/ProductsAdmin.js
@@ -1,11 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Loader } from "semantic-ui-react";
-import { HeaderPage, TableProducts } from "../../components/Admin";
+import {
+  HeaderPage,
+  TableProducts,
+  AddEditProducts,
+} from "../../components/Admin";
 import { useProduct } from "../../hooks";
+import { ModalBasic } from "../../components/Common";
 
 export function ProductsAdmin() {
   const { loading, products, getProducts } = useProduct();
+  const [showModal, setshowModal] = useState(false);
+  const [titleModal, settitleModal] = useState(null);
+  const [contentModal, setcontentModal] = useState(null);
+
   useEffect(() => getProducts(), []);
+
+  const openCloseModal = () => setshowModal((prev) => !prev);
+
+  const addProduct = () => {
+    settitleModal("Agregar Producto");
+    setcontentModal(<AddEditProducts onClose={openCloseModal} />);
+    openCloseModal();
+  };
 
   return (
     <div>
@@ -13,6 +30,7 @@ export function ProductsAdmin() {
         title="Productos"
         btnTitle="Agregar Producto"
         icon="product hunt"
+        btnClick={addProduct}
       />
       {loading ? (
         <Loader active inline="centered">
@@ -21,6 +39,12 @@ export function ProductsAdmin() {
       ) : (
         <TableProducts products={products} />
       )}
+      <ModalBasic
+        show={showModal}
+        onClose={openCloseModal}
+        title={titleModal}
+        children={contentModal}
+      />
     </div>
   );
 }
